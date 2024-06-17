04ae8a5032adc772d24bc0791e14fc4de6be1613
Stack <stack@Roopeshs-MacBook-Air.local>
2024-06-17 11:00:48 +0530

    This is the source code

---
+++ src/dummy-data.js
@@ -0,0 +1,63 @@
+export const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Mashi',
+      description: 'Finest hyderabadi biriyani',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Fried rice',
+      description: 'schezwan fried rice',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Rominos pizza',
+      description: 'to... tasty',
+      price: 18.99,
+    },
+    {
+      id: 'm5',
+      name: 'Dharbar biriyani',
+      description: 'hot and spicy..',
+      price: 18.99,
+    },
+    {
+      id: 'm6',
+      name: 'Chocolavacake',
+      description: 'too...choclaty & delicious',
+      price: 10.99,
+    },
+    {
+      id: 'm7',
+      name: 'Chicken roll',
+      description: 'affordabble & delicious',
+      price: 8.99,
+    },
+    {
+      id: 'm8',
+      name: 'Panner roll',
+      description: 'affordabble & delicious',
+      price: 7.99,
+    },
+    {
+      id: 'm9',
+      name: 'Biriyani Badshah',
+      description: 'too... tasty',
+      price: 16.99,
+    },
+    {
+      id: 'm10',
+      name: 'Panner butter masala',
+      description: 'Creamy goodness: paneer perfection',
+      price: 18.99,
+    },
+    
+  ];
